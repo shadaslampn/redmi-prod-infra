0033c7968ea7ed4c9aa65d6b1c7238025aa0e4d1
shad <aslam@shadops.com>
2023-12-07 17:27:07 +0530

    remote access sg created

--- main.tf
+++ main.tf
@@ -46,3 +46,32 @@ resource "aws_security_group" "http_access" {
     Name = "${var.project_name}-${var.project_env}-http_access"
   }
 }
+
+#--------------------------------#
+# Security Groups- remote_access #
+#--------------------------------#
+
+resource "aws_security_group" "remote_access" {
+  name        = "${var.project_name}-${var.project_env}-remote_access"
+  description = "${var.project_name}-${var.project_env}-remote_access"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-remote_access"
+  }
+}
