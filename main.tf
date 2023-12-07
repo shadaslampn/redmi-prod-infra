3461dd71624c82892f640f98c4d024b4bac99193
shad <aslam@shadops.com>
2023-12-07 17:24:18 +0530

    web access sg created

--- main.tf
+++ main.tf
@@ -9,3 +9,40 @@ resource "aws_key_pair" "auth_key" {
     Name = "${var.project_name}-${var.project_env}"
   }
 }
+
+#------------------------------#
+# Security Groups- http_access #
+#------------------------------#
+
+resource "aws_security_group" "http_access" {
+  name        = "${var.project_name}-${var.project_env}-http_access"
+  description = "${var.project_name}-${var.project_env}-http_access"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-http_access"
+  }
+}
