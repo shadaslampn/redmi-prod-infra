9e2c01614cf29eb20d6629c0529c2b4da77aeab6
shadops <shadops@shadops.com>
2023-12-13 11:22:34 +0000

    prometheus port

--- main.tf
+++ main.tf
@@ -41,6 +41,14 @@ resource "aws_security_group" "http_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  #prometheus-port
+  ingress {
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
